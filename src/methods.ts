d58d9769c962d08ed47b489b0acccacaff3dcfd7
nipud0s <nipudos-01@yahoo.com>
2023-05-01 17:58:06 +0800

    feat: track price

--- src/methods.ts
+++ src/methods.ts
@@ -1,10 +1,27 @@
-import { base58Decode } from "@massalabs/massa-web3/dist/utils/Xbqcrypto";
+import { Args, strToBytes } from "@massalabs/massa-web3";
+import { web3Client } from "./client";
 
-export const isAddressValid = (address: string) =>
-    address.startsWith("A1") && (address.length === 52 || address.length === 51);
+const REAL_ID_SHIFT = 2 ** 17;
 
-export const getBytesPublicKey = (publicKey: string): Uint8Array => {
-    const publicKeyVersionBase58Decoded: Buffer = base58Decode(publicKey.slice(1));
-    const publicKeyBase58Decoded = publicKeyVersionBase58Decoded.slice(1);
-    return publicKeyBase58Decoded;
-};
+export const getPriceFromId = (id: number, binStep: number): number =>
+    (1 + binStep / 10000) ** (id - REAL_ID_SHIFT);
+
+export const getIdFromPrice = (price: number, binStep: number): number =>
+    Math.round(Math.log(price) / Math.log(1 + binStep / 10000) + REAL_ID_SHIFT);
+
+export const getBinStep = (pairAddress: string): Promise<number | undefined> =>
+    web3Client
+        .publicApi()
+        .getDatastoreEntries([
+            {
+                address: pairAddress,
+                key: strToBytes("FEES_PARAMETERS"),
+            },
+        ])
+        .then((entries) => {
+            if (!entries[0].final_value) return;
+
+            const args = new Args(entries[0].final_value);
+            const binStep = args.nextU32();
+            return binStep;
+        });
--- src/trpc.ts
+++ src/trpc.ts
@@ -2,8 +2,12 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import { string, z } from "zod";
 import { prisma } from "./db";
+import type { Price } from "@prisma/client";
 
-export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+export const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({
     req,
     res,
     prisma,
@@ -45,18 +49,54 @@ export const appRouter = t.router({
                 take,
             });
         }),
-    getRecentSwaps: t.procedure.input(z.string()).query(async ({ input, ctx }) => {
-        return ctx.prisma.swap.findMany({
-            where: {
-                poolAddress: input,
-            },
-            orderBy: {
-                timestamp: "desc",
-            },
-            take: 10,
-        });
-    }),
-    // getPoolDistribution: t.procedure.input(z.string()).query(async ({ input, ctx }) => {
+    getRecentSwaps: t.procedure
+        .input(z.string())
+        .query(async ({ input, ctx }) => {
+            return ctx.prisma.swap.findMany({
+                where: {
+                    poolAddress: input,
+                },
+                orderBy: {
+                    timestamp: "desc",
+                },
+                take: 10,
+            });
+        }),
+    getPrice: t.procedure
+        .input(
+            z.object({
+                address: z.string(),
+                take: z.number(),
+            })
+        )
+        .query(async ({ input, ctx }) => {
+            const { address, take } = input;
+            return ctx.prisma.price
+                .findMany({
+                    where: {
+                        address,
+                    },
+                    take,
+                    orderBy: {
+                        date: "asc",
+                    },
+                })
+                .then((prices) => {
+                    const res: Price[] = [];
+                    prices.forEach((price, i) => {
+                        if (take === 168) {
+                            if (i % 6 === 0) res.push(price);
+                            return;
+                        }
+                        if (take === 720) {
+                            if (i % 24 === 0) res.push(price);
+                            return;
+                        }
+                        res.push(price);
+                    });
+                    return res;
+                });
+        }),
 });
 
 export const expressMiddleware = trpcExpress.createExpressMiddleware({
