c74be246ef4e24d64593b73d183fbe151cd1dd56
Dusa Protocol VPS <nipudos-01@yahoo.com>
2023-06-13 13:16:35 +0000

    fix factory address

--- common/contracts.ts
+++ common/contracts.ts
@@ -1,5 +1,4 @@
-export const factorySC =
-  "AS12HvhDnJgJqL5gob9GZY5nGXTC1rAbCfKUicYYoiyaBQ8CUrY8F";
+export const factorySC = "AS1D4nGcXw1MYuDCkLGXjHr5BmMbzPHvAaaiG91tmS1ytp32wU9g";
 export const dcaSC = "AS1vEajKEB1dwmyF5oVwqHSDzxxYGgFZBpipSjvuPQ2sGgPtg4H2";
 
 export const usdcSC = "AS1jvuXwyP77M5ydtRKxJvp4EdtSWuSG8pohfuEDQLwqqgmvkjJ5";
