7e06706af02b94197749b81bdfde6f0604adce7b
JoeTheCookie217 <56717228+JoeTheCookie217@users.noreply.github.com>
2023-11-02 14:47:15 +0100

    update sdk

--- common/contracts.ts
+++ common/contracts.ts
@@ -1,8 +1,8 @@
 import {
-  ChainId,
   DCA_MANAGER_ADDRESS,
   LB_FACTORY_ADDRESS,
   LB_ROUTER_ADDRESS,
+  LIMIT_ORDER_MANAGER_ADDRESS,
   USDC,
   WMAS,
 } from "@dusalabs/sdk";
@@ -11,6 +11,7 @@ import { CHAIN_ID } from "./client";
 export const routerSC = LB_ROUTER_ADDRESS[CHAIN_ID];
 export const factorySC = LB_FACTORY_ADDRESS[CHAIN_ID];
 export const dcaSC = DCA_MANAGER_ADDRESS[CHAIN_ID];
+export const orderSC = LIMIT_ORDER_MANAGER_ADDRESS[CHAIN_ID];
 
 export const usdcSC = USDC[CHAIN_ID].address;
 export const wmasSC = WMAS[CHAIN_ID].address;
